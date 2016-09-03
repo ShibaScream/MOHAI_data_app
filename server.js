c19259de58ab7a627220ec8379648f96389dfadb
Dan Peters <daniel.r.peters@icloud.com>
2016-09-03 10:02:23 -0700

    connected the postgres db to our app

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+  app = express(),
+  pg = require('pg');
+
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
 
 app.get('/', function(req, res) {
   res.send('hello world');
