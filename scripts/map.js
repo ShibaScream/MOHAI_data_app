6e1487e8360472227374dac5e87edb9777a6d83e
REN BURNETT <renxburnett@gmail.com>
2016-09-03 17:45:08 -0700

    started map functionality

--- scripts/map.js
+++ scripts/map.js
@@ -1,11 +1,38 @@
 (function(module){
-  var mapObj = {};
 
   mapObj.visitorLocation = function() {
   $.get('/location').done(function(data) {
-      console.log(data);
+      console.log('inside vistitor location function');
+      return data
     });
   };
 
+  // mapObj.filterLocation = function(arr) {
+  //   console.log(arr);
+  //   arr.filter(
+  //   });
+  // };
+//
   module.mapObj = mapObj;
 })(window);
+
+mapObj.visitorLocation();
+
+function initMap(result) {
+  var map;
+
+  if(result) {
+    map = new
+    google.maps.Map(document.getElementById('map'), {
+      scrollwheel: true,
+      zoom: 8
+    });
+
+    result.forEach(function(visitor) {
+      var marker = new google.maps.Marker({
+        position: {lat: visitor.location}
+      })
+    })
+    })
+  }
+}
