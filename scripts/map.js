609c62846fec7745c43958b311b4cbb860b708ae
Frank Iriarte <jurrish@gmail.com>
2016-09-04 16:30:37 -0700

    map rendering

--- scripts/map.js
+++ scripts/map.js
@@ -1,38 +1,39 @@
-(function(module){
-
-  mapObj.visitorLocation = function() {
-  $.get('/location').done(function(data) {
-      console.log('inside vistitor location function');
-      return data
-    });
-  };
-
-  // mapObj.filterLocation = function(arr) {
-  //   console.log(arr);
-  //   arr.filter(
-  //   });
-  // };
+// (function(module){
 //
-  module.mapObj = mapObj;
-})(window);
-
-mapObj.visitorLocation();
-
-function initMap(result) {
-  var map;
-
-  if(result) {
-    map = new
-    google.maps.Map(document.getElementById('map'), {
-      scrollwheel: true,
-      zoom: 8
-    });
-
-    result.forEach(function(visitor) {
-      var marker = new google.maps.Marker({
-        position: {lat: visitor.location}
-      })
-    })
-    })
-  }
-}
+//   mapObj.visitorLocation = function() {
+//   $.get('/location').done(function(data) {
+//       console.log('inside vistitor location function');
+//       return data
+//     });
+//   };
+//
+//   // mapObj.filterLocation = function(arr) {
+//   //   console.log(arr);
+//   //   arr.filter(
+//   //   });
+//   // };
+// //
+//
+//   module.mapObj = mapObj;
+// })(window);
+//
+// mapObj.visitorLocation();
+//
+// function initMap(result) {
+//   var map;
+//
+//   if(result) {
+//     map = new
+//     google.maps.Map(document.getElementById('map'), {
+//       scrollwheel: true,
+//       zoom: 8
+//     });
+//
+//     result.forEach(function(visitor) {
+//       var marker = new google.maps.Marker({
+//         position: {lat: visitor.location}
+//       })
+//     })
+//     })
+//   }
+// }
