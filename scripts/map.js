32de1e81b4dd7a856a60f14d9ca48822824baecb
REN BURNETT <renxburnett@gmail.com>
2016-09-05 00:53:10 -0700

    filtered location data from server and got google api call to work

--- scripts/map.js
+++ scripts/map.js
@@ -1,25 +1,54 @@
-// (function(module){
+(function(module){
 
-//get visitor location data
-  mapObj.visitorLocation = function() {
-  $.get('/location').done(function(data) {
-      // console.log(data.location);
-      console.log(data);
-      return data
+  mapObj = {};
+  mapObj.geoCodeJSON = [];
+  mapObj.parsedLocation = [];
+
+// get visitor location data and query google api
+  // mapObj.severReq = function(callback) {
+  //   $.get('/location').then(function(data) {
+  //       data.forEach(function(ele) {
+  //         mapObj.parsedLocation.push(ele.answertext.split(' ').join('+'));
+  //       });
+  //       console.log(mapObj.parsedLocation);
+  //   }).done(callback);
+  // };
+
+  mapObj.googleReq = function(callback) {
+    $.ajax({
+        url: '/location',
+        type: 'GET',
+        success: function(data) {
+          data.forEach(function(ele) {
+          mapObj.parsedLocation.push(ele.answertext.split(' ').join('+'));
+          console.log('hi');
+        })
+      }
+    }).done(function() {
+      mapObj.parsedLocation.forEach(function(ele) {
+        $.ajax({
+            url: 'https://maps.googleapis.com/maps/api/geocode/json?&address=' + ele + '&key=AIzaSyB3pPN8d00FXzTZOjGUHKapkreiitMwfxE',
+            type: 'GET',
+            success: function(data, message, xhr) {
+              mapObj.geoCodeJSON = data;
+              console.log(mapObj.geoCodeJSON);
+            }
+          });
+        });
     });
   };
 
-// //perform google geocoding request
-// mapObj.googleReq = function(visitor) {
-//   $.get('http://maps.googleapis.com/maps/api/geocode/outputFormat?parameters')
-// }
+  mapObj.googleReq();
 
+  module.mapObj = mapObj;
+})(window);
 
-  // module.mapObj = mapObj;
-// })(window);
-
-mapObj.visitorLocation();
 //
+//perform google geocoding request and pull lat/lon
+// mapObj.mapRenderReq = function(data){
+//   console.log(data);
+//   };
+
 // //render map
 // function initMap(result) {
 //   var map;
