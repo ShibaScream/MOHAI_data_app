4a92d9c6653573f138feaade31a5b287273feba7
REN BURNETT <renxburnett@gmail.com>
2016-09-05 16:06:48 -0700

    got everything working except map rendering lol

--- scripts/map.js
+++ scripts/map.js
@@ -1,8 +1,11 @@
-(function(module){
+(function(module) {
 
   mapObj = {};
   mapObj.geoCodeJSON = [];
   mapObj.parsedLocation = [];
+
+//** dont forget to delete
+  mapObj.dummyData = ['afghanistan', 'iraq', 'india'];
 
 // get visitor location data and query google api
   mapObj.googleReq = function(callback) {
@@ -12,50 +15,61 @@
         success: function(data) {
           data.forEach(function(ele) {
           mapObj.parsedLocation.push(ele.answertext.split(' ').join('+'));
-          console.log('hi');
+          console.log('successful heroku server retrieval');
         })
       }
-    }).done(function() {
-      mapObj.parsedLocation.forEach(function(ele) {
+    }).then(function() {
+      mapObj.dummyData.forEach(function(ele) {  ////dont forget to change dummy data back
         $.ajax({
             url: 'https://maps.googleapis.com/maps/api/geocode/json?&address=' + ele + '&key=AIzaSyB3pPN8d00FXzTZOjGUHKapkreiitMwfxE',
             type: 'GET',
             success: function(data, message, xhr) {
-              mapObj.geoCodeJSON = data;
-              console.log(mapObj.geoCodeJSON);
+              console.log('successful google JSON retrieval');
             }
-          });
+          }).then(function(data) {
+              data.results.forEach(function(data) {
+                // console.log(data.geometry);
+                mapObj.geoCodeJSON.push(data.geometry);
+              });
+            }).then(mapObj.mapRender().one());
         });
-    });
+      });
+  };
+
+  //pull lat/lon from geocodeJSON and render map
+  mapObj.mapRender = function() {
+    var map;
+
+    console.log('inside map render');
+
+    if (mapObj.geoCodeJSON.length > 0) {
+      map = new
+      google.maps.Map(document.getElementById('i_frame'), {
+        scrollwheel: true,
+        zoom: 4
+      });
+
+      mapObj.geoCodeJSON.forEach(function(ele) {
+        var marker = new google.maps.Marker({
+          position: ele.location,
+          map: map,
+          animation: google.maps.Animation.DROP,
+          icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
+        });
+        //set center to seattle lat/lng
+        map.setCenter({lat: 47.608013, lng: -122.335167});
+      });
+    } else {
+      // if no data then create a map of tasmania because... ya know.
+      map = new google.maps.Map(document.getElementById('i_frame'), {
+        center: {lat: -41.4545196, lng: 145.9706648},
+        scrollwheel: true,
+        zoom: 6
+      });
+    }
   };
 
   module.mapObj = mapObj;
 })(window);
 
 mapObj.googleReq();
-
-
-//perform google geocoding request and pull lat/lon
-// mapObj.mapRenderReq = function(data){
-//   console.log(data);
-//   };
-
-// //render map
-// function initMap(result) {
-//   var map;
-//
-//   if(result) {
-//     map = new
-//     google.maps.Map(document.getElementById('map'), {
-//       scrollwheel: true,
-//       zoom: 8
-//     });
-//
-//     result.forEach(function(visitor) {
-//       var marker = new google.maps.Marker({
-//         position: {lat: visitor.location}
-//       })
-//     })
-//     })
-//   }
-// }
