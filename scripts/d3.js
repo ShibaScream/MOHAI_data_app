3b11caa615ea23ad62ca3ee9a590aa7dd62d3a34
Dan Peters <daniel.r.peters@icloud.com>
2016-09-05 15:09:11 -0700

    fixed some font bugs, moved font folder into styles folder, and add correct font files

--- scripts/d3.js
+++ scripts/d3.js
@@ -1,4 +1,12 @@
 (function(module) {
   visualization = {};
-  
+
+  visualization.ages = function() {
+    $.get('/ages', function(data) {
+      console.log(data);
+    });
+  };
+
+  module.visualization = visualization;
+
 }(window));
