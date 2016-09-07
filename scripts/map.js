42a4d4f1ec8964c376ae78fcda0faeb61effec6c
Dan Peters <daniel.r.peters@icloud.com>
2016-09-06 21:07:22 -0700

    refactored the google maps code

--- scripts/map.js
+++ scripts/map.js
@@ -3,75 +3,73 @@
   mapObj = {};
   mapObj.geoCodeJSON = [];
   mapObj.parsedLocation = [];
+  mapObj.markers = [];
 
 //** dont forget to delete
   // mapObj.dummyData = ['afghanistan', 'iraq', 'india'];
 
 // get visitor location data and query google api
   mapObj.googleReq = function(callback) {
-    $.ajax({
-        url: '/location',
-        type: 'GET',
-        success: function(data) {
-          data.forEach(function(ele) {
-          mapObj.parsedLocation.push(ele.answertext.split(' ').join('+'));
-          console.log('successful heroku server retrieval');
-        })
-      }
-    }).then(function() {
-      mapObj.parsedLocation.forEach(function(ele) {  ////dont forget to change dummy data back to parsedLocation
-        $.ajax({
-            url: 'https://maps.googleapis.com/maps/api/geocode/json?&address=' + ele + '&key=AIzaSyB3pPN8d00FXzTZOjGUHKapkreiitMwfxE',
-            type: 'GET',
-            success: function(data, message, xhr) {
-              console.log('successful google JSON retrieval');
-            }
-          }).then(function(data) {
-              data.results.forEach(function(data) {
-                // console.log(data.geometry);
-                mapObj.geoCodeJSON.push(data.geometry);
-              });
-            })
-        });
+    this.mapRender();
+    $.get('/location', function(data) {
+      mapObj.parsedLocation = data.map(function(d) {
+        if (d.answertext === 'Washington') {
+          d.answertext += '+State';
+        };
+        return d.answertext.replace(/\s+/g, '+');
       });
+    }).then(function() {
+      mapObj.geoCodeJSON = mapObj.parsedLocation.map(function(location) {
+        // because Google gets mad
+        setTimeout(function() {
+          mapObj.geocoder.geocode({'address': location}, function(results, status) {
+            if (status === 'OK') {
+              console.log(status);
+              if (results[0]) {
+                console.log(results[0].geometry.location);
+                mapObj.addMarker(results[0].geometry.location);
+              } else {
+                console.error('no results found');
+              }
+            } else {
+              console.error('Geocoder failed due to: ' + status);
+            }
+          });
+        }, 100000);
+      });
+    });
   };
 
   //pull lat/lon from geocodeJSON and render map
   mapObj.mapRender = function() {
-    var map;
+    mapObj.map;
+    mapObj.geocoder;
 
     console.log('inside map render');
 
-    if (mapObj.geoCodeJSON.length > 0) {
-      map = new
-      google.maps.Map(document.getElementById('i_frame'), {
-        scrollwheel: true,
-        zoom: 3
-      });
+    mapObj.map = new
+    google.maps.Map(document.getElementById('i_frame'), {
+      scrollwheel: true,
+      zoom: 2,
+      center: {lat: 47.608013, lng: -122.335167}
+    });
 
-      mapObj.geoCodeJSON.forEach(function(ele) {
+    mapObj.geocoder = new google.maps.Geocoder;
 
-        var marker = new google.maps.Marker({
-          position: ele.location,
-          map: map,
-          animation: google.maps.Animation.DROP,
-          icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
-        });
-        //set center to seattle lat/lng
-        map.setCenter({lat: 47.608013, lng: -122.335167});
-      });
-    } else {
-      // if no data then create a map of tasmania because... ya know.
-      map = new google.maps.Map(document.getElementById('i_frame'), {
-        center: {lat: -41.4545196, lng: 145.9706648},
-        scrollwheel: true,
-        zoom: 6
-      });
-    }
+  };
+
+  mapObj.addMarker = function(location) {
+    var marker = new google.maps.Marker({
+      // fix position
+      position: location,
+      map: mapObj.map,
+      animation: google.maps.Animation.DROP,
+      icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
+    });
+
+    this.markers.push(marker);
+
   };
 
   module.mapObj = mapObj;
 })(window);
-
-mapObj.googleReq();
-setTimeout(function() { mapObj.mapRender(); }, 1300);
