ca9d6dcebb6e4e33b33abe6ee925ab709f6ed36c
Dan Peters <daniel.r.peters@icloud.com>
2016-09-07 19:53:16 -0700

    modified location query

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ pg.connect(process.env.DATABASE_URL, function(err, client) {
   console.log('Connected to postgres! Getting schemas...');
 });
 
+// app.put()
+
 // Get data by type??
 app.get('/countvisitors', function(req, res){
   var conString = process.env.DATABASE_URL || null;
@@ -35,13 +37,13 @@ app.get('/location', function(req, res){
 
   client.connect(function (err) {
     if (err) throw err;
-    client.query('SELECT COUNT(DISTINCT visitoranswer.Visitorpollid) "Count", answer.Answertext, question.Questiontext, poll.Polltext ' +
+    client.query('SELECT COUNT(DISTINCT visitoranswer.Visitorpollid) "Count", answer.Answertext, answer.country, answer.state, answer.lat, answer.lng, question.Questiontext, poll.Polltext ' +
     	'FROM visitoranswer INNER JOIN question ON visitoranswer.Questionid = question.QuestionID ' +
       	'INNER JOIN answer ON question.Questionid = answer.Questionid AND visitoranswer.Answerid = answer.Answerid ' +
       	'INNER JOIN poll ON poll.Pollid = question.Pollid ' +
       'WHERE poll.Pollid = 7 AND ' +
     	 'question.Questionid = 7005 ' +
-      'GROUP BY poll.polltext, question.Questiontext, answer.Answertext ',
+      'GROUP BY poll.polltext, question.Questiontext, answer.Answertext, answer.country, answer.state, answer.lat, answer.lng',
       function(err, result) {
         if (err) throw err;
         client.end(function (err) {
