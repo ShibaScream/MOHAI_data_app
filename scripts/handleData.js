8f555512bc6595cbd4375a692f36fd19ec9dcdbe
REN BURNETT <renxburnett@gmail.com>
2016-09-09 19:47:22 -0700

    failed miserably

--- scripts/handleData.js
+++ scripts/handleData.js
@@ -1,35 +1,29 @@
 (function(module) {
   dataHandler = {};
 
-  dataHandler.populateFilters = function() {
-    $.get('/ages', function(data) {
-        var optionTag = '<option value="' + data[0].questiontext + '">' + data[0].questiontext + '</option>';
-        $('#question_selection').append(optionTag);
-        console.log(data);
-    });
-    //look thru server requests and append str value to option tags
-    $.get('/data/why_come', function(data) {
-        console.log(data);
+  dataHandler.all = [];
+  dataHandler.rain = [];
+
+  dataHandler.getAllData = function() {
+    $.get('/data/all', function(data) {
+      dataHandler.all = data;
     });
   };
 
-  dataHandler.displayGraph = function() {
-    $('#question_selection').on('change', function() {
-      if ($(this).val() === 'Age') {
-        $.get('/ages', function(data) {
-          visualization.verticalBarChart(data);
-          console.log('inside display graph');
-        });
-      } else if {
-        //continue long if/else blocks
-      }
-      // $('#visual_data_representation')
-  });
-};
+//this does not work
+  dataHandler.populateFilters = function(arr) {
+      dataHandler.rain = arr.map(function(ele) {
+      return ele.questiontext == 'How do you deal with the rain?';
+    })
+    .filter(function(question, index, arr) {
+      return arr.indexOf(question) === index;
+    });
+  };
+  //this does not work
+
 
   module.dataHandler = dataHandler;
 })(window);
 
-
+dataHandler.getAllData();
 dataHandler.populateFilters();
-dataHandler.displayGraph();
