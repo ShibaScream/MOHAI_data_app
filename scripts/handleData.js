eace6c1b3d5564033a2b79b017115b38901ebcc1
REN BURNETT <renxburnett@gmail.com>
2016-09-09 21:42:38 -0700

    dan saved us

--- scripts/handleData.js
+++ scripts/handleData.js
@@ -2,28 +2,64 @@
   dataHandler = {};
 
   dataHandler.all = [];
-  dataHandler.rain = [];
 
-  dataHandler.getAllData = function() {
+  // private function to check index for de-duping data
+  var indexOfObject = function(arr, propCheck, obj) {
+    for (var i = 0; i < arr.length; i++) {
+      // console.log(obj[propCheck]);
+      if (arr[i][propCheck] === obj[propCheck]) {
+        return i;
+      }
+    }
+    return -1;
+  };
+
+  var propertyMatch = function(arr, property, obj) {
+
+  };
+
+  dataHandler.init = function() {
     $.get('/data/all', function(data) {
+      console.log('getting data');
+      // console.log(data);
       dataHandler.all = data;
+      dataHandler.populateQuestions(dataHandler.all);
     });
   };
 
-//this does not work
-  dataHandler.populateFilters = function(arr) {
-      dataHandler.rain = arr.map(function(ele) {
-      return ele.questiontext == 'How do you deal with the rain?';
-    })
-    .filter(function(question, index, arr) {
-      return arr.indexOf(question) === index;
-    });
-  };
-  //this does not work
+  dataHandler.populateQuestions = function(data) {
+    var property = 'questiontext';
+    var uniquestions = data
+      .filter(function(obj, i, arr) {
+        return indexOfObject(arr, property, obj) === i;
+      }, [])
+      .map(function(obj){
+        return obj[property]
+      });
 
+      uniquestions.forEach(function(ele) {
+        var optionTag = '<option value="' + ele + '">' + ele + '</option>';
+        $('#question_selection').append(optionTag);
+      });
+    };
+
+
+    dataHandler.populateGraphs = function(data) {
+      $('#question_selection').on('change', function(data) {
+       return $(this).value();
+      });
+      // var property = 'questiontext';
+      //
+      //   .filter(function(arr, property, obj) {
+      //     return propertyMatch(arr, property, obj);
+      //   }, [])
+      // });
+      // console.log($(this).val());
+    };
+
+
+  dataHandler.init();
+  dataHandler.populateGraphs();
 
   module.dataHandler = dataHandler;
 })(window);
-
-dataHandler.getAllData();
-dataHandler.populateFilters();
